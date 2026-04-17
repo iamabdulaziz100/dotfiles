f3bbe4386f6d9b5d34590d7c9826a793925b2422
Abdul Aziz <iamabdulaziz100@gmail.com>
2026-04-17 06:10:09 +0600

    updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,31 +1,40 @@
-### EXPORT ###
-set fish_greeting # Supresses fish"s intro message
-set -gx PATH $PATH $HOME/.local/bin/
-set TERM xterm-256color # Sets the terminal type
-set -x EDITOR nvim # $EDITOR use neovim in terminal
-set -x MANPAGER "nvim +Man!" # "nvim" as manpager
+### ===== ENVIRONMENT =====
 
-# Set up fzf key bindings
+# Disable greeting
+set -g fish_greeting
+
+# PATH
+fish_add_path ~/.local/bin
+
+# Terminal & editor
+set -gx TERM xterm-256color
+set -gx EDITOR nvim
+set -gx MANPAGER "nvim +Man!"
+
+
+### ===== TOOLS =====
+
 fzf --fish | source
-
-# "Zoxide" better "cd"
 zoxide init fish | source
 
-### FUNCTIONS ###
 
-### SET  VI MODE  In Fish Shell ###
+### ===== KEYBINDINGS =====
+
 function fish_user_key_bindings
     fish_vi_key_bindings
 end
 
-### ALIASES ###
 
-# changing "ls" to "eza"
+### ===== ALIASES =====
+
+# eza
 alias l="eza --icons --color=always"
 alias ls="eza -a --icons --color=always --group-directories-first --git"
 alias lt="eza -a --long --tree --icons --color=always --group-directories-first --git --level=1"
 
-# changing "cd" to "zoxide"
+# zoxide
 alias cd="z"
 
+# Starship prompt
+set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
 starship init fish | source
